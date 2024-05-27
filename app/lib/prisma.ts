5c3d869bcb41bc8d60e123baf8fe4be4b62ed6ca
MizumuraSyungo <shungo.mizumura@systemi.co.jp>
2024-05-27 07:16:17 +0000

    tuika

---
+++ app/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+prisma = new PrismaClient();
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+}
+
+prisma = globalForPrisma.prisma;
+
+export default prisma;
